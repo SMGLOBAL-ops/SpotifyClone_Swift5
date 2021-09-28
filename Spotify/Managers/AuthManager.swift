e00abae25f815c1eb47ef44d010cf3b2ec8401e2
Saif Mustafa <68763259+SMGLOBAL-ops@users.noreply.github.com>
2021-09-28 10:36:01 +0100

    Added tokenAPIURI and token exchange logic

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -13,6 +13,7 @@ final class AuthManager {
     struct Constants {
         static let clientID = "905f411dc49c47208b95b480c236159c"
         static let clientSecret = "aff83f273b0f45ffa92d9fe31cc25fbc"
+        static let tokenAPIURL = "https://accounts.spotify.com/api/token"
     }
     
     private init() {}
@@ -48,11 +49,42 @@ final class AuthManager {
     public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
         
         // Get Token
-    
+        guard let url = URL(string: Constants.tokenAPIURL) else {
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(false)
+                return
+            }
+            
+            // convert to JSON
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                
+                
+                print("SUCCESS: \(json)")
+            }
+            
+            catch {
+                print(error.localizedDescription)
+                completion(false)
+                
+            }
+            
+            
+        }
+        task.resume()
     }
     
     private func cacheToken() {
         
     }
+    
 }
 
